c11cc278752b54cbf94d32245796f22d4eaaf1f9
prashantlord <prashants2062@gmail.com>
2026-03-27 22:25:52 +0545

    Simple Res Api

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 import express from "express";
+import fs from "fs";
+import mockData from "./MOCK_DATA.json" with { type: "json" };
+import { json } from "stream/consumers";
+
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send(`Hello to the home page ${req.query.name}`);
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+      ${mockData.map((item) => `<li>${item.first_name}</li>`).join("")}
+    </ul>
+  `;
+  res.send(html);
 });
 
-app.get("/about", (req, res) => {
-  res.send(`hello in the about page ${req.query.name}`);
-});
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    res.json(mockData);
+  })
+  .post((req, res) => {})
+  .put((req, res) => {})
+  .patch((req, res) => {})
+  .delete((req, res) => {});
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
